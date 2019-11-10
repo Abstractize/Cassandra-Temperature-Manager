afa7496010be803ca1f76bf0a4bd49968dee2066
Gabriel Abarca Aguilar <gababarca@hotmail.com>
2019-11-10 17:53:32 -0600

    [Add] Pre-files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cassandra_Temperature_Manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
